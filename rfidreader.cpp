0bd962a889fb3225906e68d10a0db84dcf00afee
Can <72414463+canozcano@users.noreply.github.com>
2024-06-28 22:46:36 +0300

    Add files via upload

---
+++ rfidreader.cpp
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  SPI.begin();         // Initialize SPI bus
+  mfrc522.PCD_Init();  // Initialize MFRC522
+  Serial.println("RFID okuyucu hazır.");
+}
+
+void loop() {
+  // Check if a new card is present
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Print UID
+  Serial.print("Kart Bulundu ID: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+
+  // Halt PICC
+  mfrc522.PICC_HaltA();
+
+  // Stop encryption on PCD
+  mfrc522.PCD_StopCrypto1();
+}
